e1826bd71c544662931010bcf8cb8dc001c06021
dannc <dannc.sao@gmail.com>
2021-05-27 14:49:11 +0700

    Fix method set is not corresponds to psr.
    reorder public non-interface methods.

--- src/FileSystemCache.php
+++ src/FileSystemCache.php
@@ -55,19 +55,25 @@ class FileSystemCache implements CacheInterface
     }
 
     /**
-     * Store an item
-     * @param string $key The key under which to store the value.
-     * @param mixed $value The value to store.
-     * @param integer $lifetime The expiration time, defaults to 3600
-     * @return boolean
-     * @throws \Psr\SimpleCache\InvalidArgumentException
+     * @inheritDoc
      */
-    public function set($key, $value, $lifetime = 3600)
+    public function set($key, $value, $ttl = null)
     {
-        if (!$this->isKey($key))
+        if (!$this->isKey($key)) {
             return false;
+        }
 
-        if ($data = json_encode(array('lifetime' => time() + $lifetime, 'data' => $value))) {
+        $timeToLive = $ttl;
+        if ($ttl === null) {
+            $timeToLive = 3600;
+        } elseif ($ttl instanceof \DateInterval) {
+            $reference = new \DateTimeImmutable();
+            $endTime = $reference->add($ttl);
+            $timeToLive = $endTime->getTimestamp() - $reference->getTimestamp();
+        }
+
+
+        if ($data = json_encode(array('lifetime' => time() + $timeToLive, 'data' => $value))) {
             if (file_put_contents($this->cacheDirectory . $key, $data) !== false) {
                 $this->cache[$key] = $data;
                 return true;
@@ -78,27 +84,7 @@ class FileSystemCache implements CacheInterface
     }
 
     /**
-     * set a new expiration on an item
-     * @param string $key The key under which to store the value.
-     * @param integer $lifetime The expiration time, defaults to 3600
-     * @return boolean
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     */
-    public function touch(string $key, int $lifetime = 3600): bool
-    {
-        if ($data = $this->get($key)) {
-            return $this->set($key, $data, $lifetime);
-        }
-
-        return false;
-    }
-
-    /**
-     * returns the item that was previously stored under the key
-     * @param string $key The key of the item to retrieve.
-     * @param  mixed $default The default value (see @return)
-     * @return mixed Returns the value stored in the cache or $default otherwise
-     * @throws \Psr\SimpleCache\InvalidArgumentException
+     * @inheritDoc
      */
     public function get($key, $default = null)
     {
@@ -135,10 +121,7 @@ class FileSystemCache implements CacheInterface
     }
 
     /**
-     * Delete an item
-     * @param string $key The key to be deleted.
-     * @return boolean Returns TRUE on success or FALSE on failure
-     * @throws \Psr\SimpleCache\InvalidArgumentException
+     * @inheritDoc
      */
     public function delete($key)
     {
@@ -146,6 +129,59 @@ class FileSystemCache implements CacheInterface
             return false;
 
         return $this->deleteFile($this->cacheDirectory . $key);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function clear()
+    {
+        return $this->deleteByPattern('*');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getMultiple($keys, $default = null)
+    {
+        $values = array();
+        foreach ($keys AS $key) {
+            $values[$key] = $this->get($key, $default);
+        }
+        return $values;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function setMultiple($values, $ttl = null)
+    {
+        $return = false;
+        foreach ($values AS $key => $value) {
+            $return = $this->set($key, $value, $ttl) || $return;
+        }
+        return $return;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function deleteMultiple($keys)
+    {
+        $return = false;
+        foreach ($keys AS $key) {
+            $values[$key] = $this->delete($key);
+        }
+        return $return;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function has($key)
+    {
+        $value = $this->get($key);
+        return !empty($value);
     }
 
     /**
@@ -159,98 +195,19 @@ class FileSystemCache implements CacheInterface
     }
 
     /**
-     * Wipes clean the entire cache's keys.
-     *
-     * @return bool True on success and false on failure.
-     */
-    public function clear()
-    {
-        return $this->deleteByPattern('*');
-    }
-
-    /**
-     * Obtains multiple cache items by their unique keys.
-     *
-     * @param iterable $keys A list of keys that can obtained in a single operation.
-     * @param mixed $default Default value to return for keys that do not exist.
-     *
-     * @return iterable A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
-     *
+     * set a new expiration on an item
+     * @param string $key The key under which to store the value.
+     * @param integer $lifetime The expiration time, defaults to 3600
+     * @return boolean
      * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $keys is neither an array nor a Traversable,
-     *   or if any of the $keys are not a legal value.
      */
-    public function getMultiple($keys, $default = null)
+    public function touch(string $key, int $lifetime = 3600): bool
     {
-        $values = array();
-        foreach ($keys AS $key) {
-            $values[$key] = $this->get($key, $default);
+        if ($data = $this->get($key)) {
+            return $this->set($key, $data, $lifetime);
         }
-        return $values;
-    }
 
-    /**
-     * Persists a set of key => value pairs in the cache, with an optional TTL.
-     *
-     * @param iterable $values A list of key => value pairs for a multiple-set operation.
-     * @param null|int|\DateInterval $ttl Optional. The TTL value of this item. If no value is sent and
-     *                                       the driver supports TTL then the library may set a default value
-     *                                       for it or let the driver take care of that.
-     *
-     * @return bool True on success and false on failure.
-     *
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $values is neither an array nor a Traversable,
-     *   or if any of the $values are not a legal value.
-     */
-    public function setMultiple($values, $ttl = null)
-    {
-        $return = false;
-        foreach ($values AS $key => $value) {
-            $return = $this->set($key, $value, $ttl) || $return;
-        }
-        return $return;
-    }
-
-    /**
-     * Deletes multiple cache items in a single operation.
-     *
-     * @param iterable $keys A list of string-based keys to be deleted.
-     *
-     * @return bool True if the items were successfully removed. False if there was an error.
-     *
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if $keys is neither an array nor a Traversable,
-     *   or if any of the $keys are not a legal value.
-     */
-    public function deleteMultiple($keys)
-    {
-        $return = false;
-        foreach ($keys AS $key) {
-            $values[$key] = $this->delete($key);
-        }
-        return $return;
-    }
-
-    /**
-     * Determines whether an item is present in the cache.
-     *
-     * NOTE: It is recommended that has() is only to be used for cache warming type purposes
-     * and not to be used within your live applications operations for get/set, as this method
-     * is subject to a race condition where your has() will return true and immediately after,
-     * another script can remove it making the state of your app out of date.
-     *
-     * @param string $key The cache item key.
-     *
-     * @return bool
-     *
-     * @throws \Psr\SimpleCache\InvalidArgumentException
-     *   MUST be thrown if the $key string is not a legal value.
-     */
-    public function has($key)
-    {
-        $value = $this->get($key);
-        return !empty($value);
+        return false;
     }
 
     /**
